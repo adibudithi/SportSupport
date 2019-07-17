d852590a95e9688b7111691c68583f25f8363e18
Ismail Dayan <iedayan03@gmail.com>
2019-07-17 16:04:22 -0500

    Finished Registration Activity

--- app/src/main/java/com/iedayan03/sportsupport/RegisterActivity.java
+++ app/src/main/java/com/iedayan03/sportsupport/RegisterActivity.java
@@ -6,13 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String KEY_EMPTY = "";
+
+    EditText etFullName, etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etFullName = findViewById(R.id.fullName);
+        etUsername = findViewById(R.id.username);
+        etPassword = findViewById(R.id.password);
 
         Button loginBtn = findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +32,35 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void Register(View view) {
+        String fullName = etFullName.getText().toString().trim();
+        String username = etUsername.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (validateInput(fullName, username, password)) {
+            BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+            String type = "register";
+            backgroundWorker.execute(type, fullName, username, password);
+        }
+    }
+
+    private boolean validateInput(String fullName, String username, String password) {
+        if (KEY_EMPTY.equals(fullName)) {
+            etFullName.setError("Name can not be empty");
+            etFullName.requestFocus();
+            return false;
+        } else if (KEY_EMPTY.equals(username)) {
+            etUsername.setError("Username can not be empty");
+            etUsername.requestFocus();
+            return false;
+        } else if (KEY_EMPTY.equals(password)) {
+            etPassword.setError("Password can not be empty");
+            etPassword.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 }
