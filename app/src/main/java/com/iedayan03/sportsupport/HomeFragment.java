53dc26d0dcb04c78f6ad670507f063158ea82d7b
Ismail Dayan <iedayan03@gmail.com>
2019-07-18 13:58:41 -0500

    Added a ListView to display fields

--- app/src/main/java/com/iedayan03/sportsupport/HomeFragment.java
+++ app/src/main/java/com/iedayan03/sportsupport/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +16,18 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // we will need to query the fields from our database and store it as JSON
+        String[] items = {"Field1", "Field2", "Field2"};
+        ListView fieldListView = view.findViewById(R.id.fieldListView);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+            getActivity(), android.R.layout.simple_list_item_1, items
+        );
+
+        fieldListView.setAdapter(listViewAdapter);
+
+        return view;
     }
 }
--- app/src/main/java/com/iedayan03/sportsupport/MainActivity.java
+++ app/src/main/java/com/iedayan03/sportsupport/MainActivity.java
@@ -1,19 +1,16 @@
 package com.iedayan03.sportsupport;
 
-import android.app.Activity;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
+import android.view.MenuItem;
 
+import androidx.fragment.app.Fragment;
+import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.MenuItem;
-import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -31,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
 
+            // opens the fragment that was selected
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
             return true;
         }
@@ -41,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(navListener);
+
+        // display home fragment when main activity is first opened
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 
 }
